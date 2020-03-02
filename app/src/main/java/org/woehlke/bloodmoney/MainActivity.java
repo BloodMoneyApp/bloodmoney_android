1b4c956931ee9de534ac99a91dc3f6c7f3389eba
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-03-02 09:44:28 +0100

    setup

--- app/src/main/java/org/woehlke/bloodmoney/MainActivity.java
+++ app/src/main/java/org/woehlke/bloodmoney/MainActivity.java
@@ -6,6 +6,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void showAllMeasurements(){
+        setContentView(R.layout.activity_measurement_list);
+    }
+
+    public void addMeasurements(){
+        setContentView(R.layout.activity_measurement_detail);
+    }
+
+
+    public void onClick(){
+        setContentView(R.layout.activity_measurement_list);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
